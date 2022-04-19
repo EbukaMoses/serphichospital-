e7e07ff1a09aedde16d1b857d680ff77b93b456d
Ebukamoses <okoyemoses83@gmail.com>
2022-04-20 00:38:33 +0100

    first commit

---
+++ gallery.php
@@ -0,0 +1,81 @@
+ <?php include("header.php")?>
+
+    <!-- ========================
+       page title 
+    =========================== -->
+    <section class="page-title page-title-layout5 bg-overlay text-center">
+      <div class="bg-img"><img src="assets/images/slide3.jpg" alt="background"></div>
+      <div class="container">
+        <div class="row">
+          <div class="col-12">
+            <h1 class="pagetitle__heading">Our Gallery</h1>
+            <nav>
+              <ol class="breadcrumb justify-content-center mb-0">
+                <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+                <li class="breadcrumb-item"><a href="about-us.html">About</a></li>
+                <li class="breadcrumb-item active" aria-current="page">Our Gallery</li>
+              </ol>
+            </nav>
+          </div><!-- /.col-xl-6 -->
+        </div><!-- /.row -->
+      </div><!-- /.container -->
+    </section><!-- /.page-title -->
+
+    <!-- ========================
+     gallery layout2
+    =========================== -->
+    <section class="gallery-layout2 pt-130 pb-90">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-12 col-md-12 col-lg-6 offset-lg-3 text-center">
+            <div class="heading mb-60">
+              <h2 class="heading__subtitle">The Best Medical And General Practice Care!</h2>
+              <h3 class="heading__title">Providing Medical Care For The Sickest In Our Community.</h3>
+            </div><!-- /heading -->
+          </div><!-- /.col-lg-6 -->
+        </div><!-- /.row -->
+        <div class="row">
+          <div class="col-sm-6 col-md-4 col-lg-4">
+            <div class="gallery-img">
+              <a class="popup-gallery-item" href="assets/images/slide.jpg"><i class="fas fa-eye"></i></a>
+              <img src="assets/images/slide.jpg" alt="gallery img">
+            </div><!-- /.gallery-img -->
+          </div><!-- /.col-lg-4 -->
+          <div class="col-sm-6 col-md-4 col-lg-4">
+            <div class="gallery-img">
+              <a class="popup-gallery-item" href="assets/images/slide2.jpg"><i class="fas fa-eye"></i></a>
+              <img src="assets/images/slide2.jpg" alt="gallery img">
+            </div><!-- /.gallery-img -->
+          </div><!-- /.col-lg-4 -->
+          <div class="col-sm-6 col-md-4 col-lg-4">
+            <div class="gallery-img">
+              <a class="popup-gallery-item" href="assets/images/slide3.jpg"><i class="fas fa-eye"></i></a>
+              <img src="assets/images/slide3.jpg" alt="gallery img">
+            </div><!-- /.gallery-img -->
+          </div><!-- /.col-lg-4 -->
+          <div class="col-sm-6 col-md-4 col-lg-4">
+            <div class="gallery-img">
+              <a class="popup-gallery-item" href="assets/images/slide4.jpg"><i class="fas fa-eye"></i></a>
+              <img src="assets/images/slide4.jpg" alt="gallery img">
+            </div><!-- /.gallery-img -->
+          </div><!-- /.col-lg-4 -->
+          <div class="col-sm-6 col-md-4 col-lg-4">
+            <div class="gallery-img">
+              <a class="popup-gallery-item" href="assets/images/slide2.jpg"><i class="fas fa-eye"></i></a>
+              <img src="assets/images/slide2.jpg" alt="gallery img">
+            </div><!-- /.gallery-img -->
+          </div><!-- /.col-lg-4 -->
+          <div class="col-sm-6 col-md-4 col-lg-4">
+            <div class="gallery-img">
+              <a class="popup-gallery-item" href="assets/images/slide6.jpg"><i class="fas fa-eye"></i></a>
+              <img src="assets/images/slide6.jpg" alt="gallery img">
+            </div><!-- /.gallery-img -->
+          </div><!-- /.col-lg-4 -->
+        </div><!-- /.row -->
+      </div><!-- /.container -->
+    </section><!-- /.gallery layout2 -->
+
+     <?php include("footer.php")?>
+</body>
+
+</html>
